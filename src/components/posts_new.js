c4886ce120f6f14addf8d1c964617fa49c454cb6
blip <b@bradenlawrence.com>
2018-01-06 21:44:23 -0500

    Add forms for tags and post content, Add initial validation logic

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -7,7 +7,8 @@ class PostsNew extends Component {
     renderTextInput = (field) => {
         return(
             <div>
-                <input type='text' { ...field.input } />
+                <h3>{ form.label }</h3>
+                <input type ='text' { ...field.input } />
             </div>
         )
     }
@@ -17,10 +18,24 @@ class PostsNew extends Component {
             <div>
                 <div>You found the new post route!</div>
                 <form>
-                    <Field name='Title' component={ this.renderTextInput } />
+                    <Field 
+                        label =     'Title'
+                        name =      'title' 
+                        component = { this.renderTextInput } 
+                    />
+                    <Field 
+                        label =     'Tags'
+                        name =      'tags' 
+                        component = { this.renderTextInput } 
+                    />
+                    <Field 
+                        label =     'Post contents'
+                        name =      'body' 
+                        component = { this.renderTextInput } 
+                    />
                 </form>
                 <div className='text-xs-right'>
-                    <Link className='btn btn-primary' to='/'>
+                    <Link className = 'btn btn-primary' to = '/' >
                         Cancel
                     </Link>
                 </div>
@@ -31,6 +46,19 @@ class PostsNew extends Component {
 
 
 const PostsNewContainer = reduxForm({
-    form: 'PostsNewForm'
+    validate(values)    {
+        const errors = {}
+        if( !values.title ) {
+            errors.title = 'Please enter a title'
+        }
+        if( !values.tags ) {
+            errors.title = 'Please enter at least one tag'
+        }
+        if( !values.body ) {
+            errors.title = 'Post contents cannot be empty'
+        }
+        return errors
+    },
+    form:               'PostsNewForm'
 })(PostsNew)
 export { PostsNewContainer }
